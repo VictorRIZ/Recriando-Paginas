fe24df79b775ccfecf4dcbcbdbd341aefada9d09
Victor <victorari856@gmail.com>
2021-09-14 14:27:18 -0300

    commit

---
+++ UtilidadesFrontEnd/utilidades.js
@@ -0,0 +1,45 @@
+/*Arquivo destinado a colocar informações, utlidades, descobertas e coisas interessantes
+sobre o JS, desde funtions, const, array, e por assim vai....*/ 
+
+// 4 Ways to remove elements in array in JS
+
+//1
+//Using pop
+
+const fruits = ['banana', 'orange', 'apple']
+fruits.pop()
+/*pop removes the last element from an array and returns it. 
+If the array is empty, undefined is returned and 
+the array is not modified.*/
+//Resumindo, ficou assim: ['banana', 'orange']
+
+//2
+//Using shift()
+
+const fruits = ['banana', 'orange', 'apple']
+fruits.shift()
+/*shift removes the first element from an array and returns it. 
+If the array is empty, 
+undefined is returned and the array is not modified.*/
+//Resumindo, ficou assim: ['orange', 'apple']
+
+//3
+//Using splice(position, quantity)
+
+const fruits = ['banana', 'orange', 'apple']
+fruits.splice(1, 1)
+/*splice Removes elements from an array and, if necessary, 
+inserts new elements in their place, 
+returning the deleted elements.*/
+//Resumindo, ficou assim: ['banana', 'apple']
+
+//4
+//Using delete
+
+const fruits = ['banana', 'orange', 'apple']
+delete fruits[2]
+console.log(fruits.length)
+/*delete deletes elements from array and objects. It's necessary 
+to put the position of the element. So, the object return undefined*/
+//Resumindo, ficou assim: ['banana', 'orange', undefined] 
+
