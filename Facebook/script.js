81ff0511528dee3ffda6712af5b7e59bcdbe0acd
Victor <victorari856@gmail.com>
2021-12-03 21:52:58 -0300

    Index + register and login

--- Facebook/script.js
+++ Facebook/script.js
@@ -15,3 +15,33 @@ const Modal = {
          .remove('active')
     }
 }
+/*Mesmo código, porém só mudei os nomes para não gerar conflito*/ 
+const ModalTwo = {
+    open(){
+       
+         document.querySelector('.main-overlay.two')
+         .classList
+         .add('active')
+    },
+    close(){
+        
+        document.querySelector('.main-overlay.two')
+         .classList
+         .remove('active')
+    }
+}
+
+const ModalThree = {
+    open(){
+        
+         document.querySelector('.main-overlay')
+         .classList
+         .add('active')
+    },
+    close(){
+        
+        document.querySelector('.main-overlay')
+         .classList
+         .remove('active')
+    }
+}
